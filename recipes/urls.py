9c1eda36fe46c9f646325e3af7a407e444acf773
Pedro Henrique <pedrox.h15z@gmail.com>
2026-03-21 17:39:45 -0300

    :sparkles: feat:  adicionar funcionalidade de favoritar receitas

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    
+    path('recipes/<int:receita_id>/', views.favoritar_receita, name='favoritar_receita'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from .models import Receita, ReceitaIngrediente
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from .models import Receita, ReceitaIngrediente, Favoritos
+
+
 
 
 def home(request):
@@ -17,6 +20,15 @@ def home(request):
 
 
     return render(request, 'recipes/recipes.html', {'dados': dados})
+
+def favoritar_receita(request, receita_id):
+
+    receita = get_object_or_404(Receita, id=receita_id)
+    usuario = User.objects.first()  
+
+    Favoritos.objects.get_or_create(usuario=usuario, receita=receita)
+
+    return redirect('home')
 
 
 
