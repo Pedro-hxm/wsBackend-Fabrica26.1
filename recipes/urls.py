2d22ec1c0102349bf12a775a964efef48e3563a8
Pedro Henrique <pedrox.h15z@gmail.com>
2026-03-21 18:39:48 -0300

    :sparkles:  implementar autenticação de usuários com cadastro, login e logout

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('recipes/<int:receita_id>/', views.favoritar_receita, name='favoritar_receita'),
+    path('cadastro/', views.cadastrar_usuario, name='cadastro'),
+    path('login/', views.login_usuario, name='login'),
+    path('logout/', views.logout_usuario, name='logout'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .models import Receita, ReceitaIngrediente, Favoritos
-
-
 
 
 def home(request):
@@ -24,12 +24,50 @@ def home(request):
     return render(request, 'recipes/recipes.html', {
         'dados': dados,
         'favoritos': favoritos})
-    
 
+
+
+def cadastrar_usuario(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if User.objects.filter(username=username).exists():
+            return render(request, 'recipes/cadastro.html', {
+                'erro': 'Este usuário já existe.'
+            })
+
+        User.objects.create_user(username=username, password=password)
+        return redirect('login')
+
+    return render(request, 'recipes/cadastro.html')
+
+
+def login_usuario(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'recipes/login.html', {
+                'erro': 'Usuário ou senha inválidos.'
+            })
+
+    return render(request, 'recipes/login.html')
+
+def logout_usuario(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
 def favoritar_receita(request, receita_id):
 
     receita = get_object_or_404(Receita, id=receita_id)
-    usuario = User.objects.first()  
 
     Favoritos.objects.get_or_create(usuario=usuario, receita=receita)
 
