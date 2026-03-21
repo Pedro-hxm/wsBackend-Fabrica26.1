a80cfcad6b110c19139436b67f339a7fb590e155
Pedro Henrique <pedrox.h15z@gmail.com>
2026-03-21 15:40:03 -0300

    :sparkles: feat: configurar painel admin para gerenciamento de dados

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ingradiente, Recipe, ReceitaIngrediente, Favoritos
+
+admin.site.register(ingradiente)
+admin.site.register(Recipe)
+admin.site.register(ReceitaIngrediente)
+admin.site.register(Favoritos)
 
 # Register your models here.
