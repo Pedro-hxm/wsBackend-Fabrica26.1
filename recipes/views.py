21b31d6dfe6dfc75757c81497018c7144c6a0614
Pedro Henrique <pedrox.h15z@gmail.com>
2026-03-21 16:25:16 -0300

    :sparkles: feat: exibir receitas com ingredientes e quantidades na interface

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .models import Recipe, ingradiente, ReceitaIngrediente, Favoritos
+
 
 def home(request):
-    return render(request, 'recipes/recipes.html')
+    receitas = Recipe.objects.all()
+
+    dados = []
+    
+    for receita in Recipe.objects.all():
+        ingradientes = ReceitaIngrediente.objects.filter(receita=receita)
+
+        dados.append({
+            'receita': receita,
+            'ingradientes': ingradientes
+        })
+
+
+    return render(request, 'recipes/recipes.html', {'dados': dados})
+    
 # Create your views here.
