6adda26d74bf04980c2666f9ca00c02b52c5b0dd
Pedro Henrique <pedrox.h15z@gmail.com>
2026-03-21 15:02:30 -0300

    :sparkles: feat: implementar modelagem completa de receitas com ingredientes e quantidades

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+# Ingredientes
+class ingradiente(models.Model):
+    nome = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.nome
+    
+#receita 
+class Recipe(models.Model):
+
+    nome = models.CharField(max_length=255)
+    categoria = models.CharField(max_length=100)
+    imagens = models.URLField()
+    instrucoes = models.TextField()
+
+    ingradientes = models.ManyToManyField(ingradiente, through='ReceitaIngrediente')
+
+    def __str__(self):
+        return self.nome
+
+# tabela de associação entre receita e ingrediente
+class ReceitaIngrediente(models.Model):
+
+    receita = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingradiente = models.ForeignKey(ingradiente, on_delete=models.CASCADE)
+    quantidade = models.CharField(max_length=100)
+    unidade = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.quantidade} {self.unidade} de {self.ingradiente.nome} para {self.receita.nome}"
+
+class Favoritos(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    receita = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.usuario.username} favoritou {self.receita.nome}"
 
 # Create your models here.
